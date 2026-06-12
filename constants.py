4c7d6c655f1234e3b75a8e795d927a10a9136710
x10102 <x10102.ot@gmail.com>
2026-06-12 02:38:33 +0200

    Add strings for AntiSpam

--- constants.py
+++ constants.py
@@ -1,4 +1,22 @@
-PM_VERB = ["slajdnout do DMs", "střelit PM", "hodit message", "napsat na Wikidotu"]
+PM_VERB = ["slajdnout do DMs",
+            "střelit PM",
+            "hodit message",
+            "napsat na Wikidotu"]
+TIMEOUTED_VERB = ["Mutenut", 
+                  "Timeoutnut", 
+                  "Zablokován", 
+                  "Odvezen do Brazílie", 
+                  "Deportován na Slovensko",
+                  "Zavřen do klece",
+                  "Zavřen do klece s výbušnými koťátky",
+                  "Zavřen do klece se sedmnácti tygry",
+                  "Svázán ve sklepě", 
+                  "Odsouzen k práci v KFC",
+                  "Odsouzen k práci v Mekáči",
+                  "Přiřazen ke Keter Duty",
+                  "Poslán krmit pstruhadla",
+                  "Vymazán z reality",
+                  "Zavřen do trouby na 180°"]
 MESSAGE_REJECT_DEFAULT = "Vaše žádost byla zamítnuta, pro více informací nás kontaktujte na našem discordu."
 MESSAGE_ACCEPT_DEFAULT = "Vaše žádost byla přijata. Vítejte v Nadaci!"
 
